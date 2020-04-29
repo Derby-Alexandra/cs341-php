a4963d4fe674a280a930cd1825ab87dbd69097d2
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-04-28 21:52:09 -0700

    starting homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,28 @@
-<?php
 
-  phpinfo();
+<!DOCTYPE html>
+<html lang="en-US">   
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS 341 | Alexandra Derby | BYU-Idaho</title>
+    <meta name="description" content="Personal Homepage">
+    <!-- <script type="text/javascript" src=""></script> -->
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>
+        <h1><a href="index.html">Alexandra Derby</a></h1>
+    </header>
 
-?>
+    <main>
+    
+        
+    </main>
+    <footer>
+        <p>
+            &copy; 2019 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a>
+        </p>
+    </footer>
+</body>    
+</html>
