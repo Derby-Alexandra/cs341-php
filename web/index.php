ccfc5c5b4ad98fb6fec1d6d3743ba742491f48d1
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-04-30 21:54:08 -0700

    update of design

--- web/index.php
+++ web/index.php
@@ -12,19 +12,17 @@
 </head>
 <body>
     <header>
-        <h1><a href="index.php">Alexandra Derby</a></h1>
-       
+        <h1><a href="index.php">Alexandra Derby</a></h1>  
     </header>
-
     <main>
-      <img src="homepageimage.jpg" alt="pretty clouds" class="imagestyles">
-    
-        
+        <div class="grid_display">
+            <p class="about_me">Welcome to my home page! One of my favorite hobbies is photography. I've included one of my dreamier shots right below this paragraph. If I could, I would spend my days wandering around taking photos just for fun! </p>
+            <p class="about_me"><a href>Click here to view my assignments!</a></p>
+        <img src="homepageimage.jpg" alt="pretty clouds" class="imagestyles">
+        </div>
     </main>
     <footer>
-        <p>
-            &copy; 2020 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a>
-        </p>
+        <p>&copy; 2020 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a></p>
     </footer>
 </body>    
 </html>
