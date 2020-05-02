98d138437be9396edf32fe5cd497ae2ae817b1f3
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-01 18:40:19 -0700

    php include file

---
+++ web/header.php
@@ -0,0 +1,8 @@
+<h1><a href="index.php">Alexandra Derby</a></h1>
+<nav>
+    <ul>
+        <li class="link_font"><a href="index.php">Home</a></li>
+        <li class="link_font"><a href="assignments.php">Assignments</a></li>
+        <li class="link_font"><a href="images.php">Images</a></li>
+    </ul>
+</nav>
--- web/index.php
+++ web/index.php
@@ -13,15 +13,7 @@
 <body>
     <div>
     <header>
-        <h1><a href="index.php">Alexandra Derby</a></h1>
-        <nav>
-            <ul>
-                <li class="link_font"><a href="index.php">Home</a></li>
-                <li class="link_font"><a href="assignments.php">Assignments</a></li>
-                <li class="link_font"><a href="images.php">Images</a></li>
-            </ul>
-        </nav>
-
+        <php? include header.php ?>
     </header>
     <main>
         <div class="grid_display">
