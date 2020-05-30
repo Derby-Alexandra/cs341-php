ef951701e4ccb6abf135ba2ff652a088825a8951
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-30 16:35:01 -0700

    scripts

--- web/weeklyproves/week6prove/newartworkconfirmation.php
+++ web/weeklyproves/week6prove/newartworkconfirmation.php
@@ -16,5 +16,5 @@ $_SESSION['dbconnection']->query("INSERT INTO artwork (artistid, imageurl, price
 header("Location: artworkinformation.php?artistid=$artistid");
 ?>
 <script>
-document.getElementById('artworkadded').innerHTML.display = block;
+document.getElementById('artworkadded').display = block;
 </script>
