1000016a5539b7001e8ff8e70895b8d6ded3b98e
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-29 22:44:35 -0700

    update

--- web/weeklyproves/week6prove/accountconfirmation.php
+++ web/weeklyproves/week6prove/accountconfirmation.php
@@ -2,9 +2,9 @@
 // Start the session
 session_start();
 // START: code to display errors
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
+//error_reporting(E_ALL);
 // END: code to display errors
 include 'dbconnect.php';
 $firstname = $_POST['firstname'];
@@ -12,7 +12,6 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $newpassword = $_POST['newpassword'];
 $stripeaccountid = $_POST['stripeaccountid'];
-$sql = $_SESSION['dbconnection']->query("INSERT INTO artist_account (email, password, stripeaccountid, firstname, lastname, accumulatedsales) VALUES ('$email', '$newpassword', '$stripeaccountid', '$firstname', '$lastname', 0)");
 ?>
 <!DOCTYPE html>
 <html lang="en-US">   
@@ -30,8 +29,18 @@ $sql = $_SESSION['dbconnection']->query("INSERT INTO artist_account (email, pass
         <?php include 'header.php';?>
     </header>
     <main>
-        <h3>Your account has been created! Please click <a href="artistlogin.php">HERE</a> to login.</h3>
-    </main>
+<?php
+        if(!$_SESSION['dbconnection']->query("INSERT INTO artist_account (email, password, stripeaccountid, firstname, lastname, accumulatedsales) VALUES ('$email', '$newpassword', '$stripeaccountid', '$firstname', '$lastname', 0)")) {
+?>
+        <h3 id="confirmation">Error - Account not created. Please click <u><a href="artistlogin.php">HERE</a></u> to try again.</h3>
+<?php 
+            else {
+?>
+                <h3 id="confirmation">"Your account has been created! Please click <u><a href="artistlogin.php">HERE</a></u> to login.";</h3>
+<?php
+            };
+?>               
+        </main>
     <footer>
         <p>&copy; 2020 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a></p>
     </footer>
