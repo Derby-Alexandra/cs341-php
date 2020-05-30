0feef5962d7d5249fd43e3cf4acaf36a82ed9661
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-30 15:41:04 -0700

    scripts

--- web/weeklyproves/week6prove/artistheader.php
+++ web/weeklyproves/week6prove/artistheader.php
@@ -1,16 +1,10 @@
 <h1>Digital Photo Sales</h1>
-        <nav>
-            <ul>
-                <li class="link_font"><a href="artistaccount.php">Home</a></li>
-                <li class="link_font"><a href="accountinformation.php">Account</a></li>
-                <li class="link_font"><a class="artistlink" href="">Artwork</a></li>
-                <li class="link_font"><a href="index.php">Logout</a></li>
-            </ul>
-        </nav>
-<!--
-    <script>
-        let userdata = JSON.parse(localStorage.getItem('userdata'))
-        document.getElementById('artistlink').href = "artworkinformation.php?artistid=" + userdata.artistid
-    </script>
--->
+<nav>
+    <ul>
+        <li class="link_font"><a href="artistaccount.php">Home</a></li>
+        <li class="link_font"><a href="accountinformation.php">Account</a></li>
+        <li class="link_font"><a class="artistlink" href="">Artwork</a></li>
+        <li class="link_font"><a href="index.php">Logout</a></li>
+    </ul>
+</nav>
     
