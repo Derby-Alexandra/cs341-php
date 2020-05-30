d8cd8f0e875c01fb2840a303a21880c91b4e7ebb
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-30 14:35:00 -0700

    scripts

--- web/weeklyproves/week6prove/artworkinformation.php
+++ web/weeklyproves/week6prove/artworkinformation.php
@@ -1,6 +1,9 @@
 <?php
 // Start the session
 session_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">   
@@ -18,6 +21,29 @@ session_start();
         <?php include 'artistheader.php';?>
     </header>
     <main>
+        <div>
+            Add New Artwork:
+            <form action="newartworkconfirmation.php" method="POST">
+                
+                <label for="imageurl">Image URL:</label>
+                <input type="text" name="imageurl" required><br>  
+                
+                <label for="price">Price:</label>
+                <input type="number" name="price" required><br>
+                
+                <label for="description">Description:</label>
+                <input type="text" name="description" required><br>
+                
+                <label for="resolution">Resolution:</label>
+                <input type="text" name="resolution" required><br>
+                
+                <input type="hidden" name="artistid" id="hiddenartistid">
+                <input type="hidden" name="artworkid" id="hiddenartworkid">
+                
+                <button type="submit">Add</button>
+            </form>
+        </div>
+        
         <div class="index_grid_images">
             <div class="artist_view_artwork">
                 Image:
@@ -38,5 +64,9 @@ session_start();
     </footer>
     <script type="text/javascript" src="scripts/imagedata.js"></script> 
     <script type="text/javascript" src="scripts/javascript.js"></script> 
+        <script>
+        let userdata = JSON.parse(localStorage.getItem('userdata'))
+        document.getElementById('hiddenartistid').value = userdata.artistid
+    </script>
 </body>
 </html>
---
+++ web/weeklyproves/week6prove/newartworkconfirmation.php
@@ -0,0 +1,53 @@
+<?php
+// Start the session
+session_start();
+// START: code to display errors
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+// END: code to display errors
+include 'dbconnect.php';
+$imageurl = $_POST['imageurl'];
+$price = $_POST['price'];
+$description = $_POST['description'];
+$resolution = $_POST['resolution'];
+$artistid = $_POST['artistid'];
+ $_SESSION['dbconnection']->query("INSERT INTO artwork (artistId,imageUrl, price, description, resolution) VALUES ('$artistid', '$imageurl', '$price', '$description', '$resolution') WHERE artistid = '$artistid'");
+?>
+<!DOCTYPE html>
+<html lang="en-US">   
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS 341 | Alexandra Derby | BYU-Idaho</title>
+    <meta name="description" content="artworkconfirmation">
+    <!-- <script type="text/javascript" src=""></script> -->
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="main.css">
+    <script>let userdata = JSON.parse(localStorage.getItem('userdata'))</script>
+</head>
+<body>
+    <header>
+        <?php include 'artistheader.php';?>
+    </header>
+    <main>
+        <div>
+<?php
+        if($oldpassword == $databasepassword && $newpassword == $newpassword2) {
+            $_SESSION['dbconnection']->query("UPDATE artist_account SET password = '$newpassword' WHERE artistid = '$artistid'");
+?> 
+            <p>Your password has been updated.</p>
+<?php     
+        } else {
+?>
+            <p>Sorry, your passwords do not match. Please try again.</p>
+<?php
+        }
+?>     
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2020 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a></p>
+    </footer>
+</body>
+</html>
