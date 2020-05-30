7c7984cec73b093142ca7fe23170e240e77b938f
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-30 14:50:04 -0700

    scripts

--- web/weeklyproves/week6prove/newartworkconfirmation.php
+++ web/weeklyproves/week6prove/newartworkconfirmation.php
@@ -13,41 +13,5 @@ $description = $_POST['description'];
 $resolution = $_POST['resolution'];
 $artistid = $_POST['artistid'];
  $_SESSION['dbconnection']->query("INSERT INTO artwork (artistid, imageurl, price, description, resolution) VALUES ('$artistid', '$imageurl', '$price', '$description', '$resolution')");
+header("Location: artworkinformation.php");
 ?>
-<!DOCTYPE html>
-<html lang="en-US">   
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CS 341 | Alexandra Derby | BYU-Idaho</title>
-    <meta name="description" content="artworkconfirmation">
-    <!-- <script type="text/javascript" src=""></script> -->
-    <link rel="stylesheet" href="normalize.css">
-    <link rel="stylesheet" href="main.css">
-    <script>let userdata = JSON.parse(localStorage.getItem('userdata'))</script>
-</head>
-<body>
-    <header>
-        <?php include 'artistheader.php';?>
-    </header>
-    <main>
-        <div>
-<?php
-        if($oldpassword == $databasepassword && $newpassword == $newpassword2) {
-            $_SESSION['dbconnection']->query("UPDATE artist_account SET password = '$newpassword' WHERE artistid = '$artistid'");
-?> 
-            <p>Your password has been updated.</p>
-<?php     
-        } else {
-?>
-            <p>Sorry, your passwords do not match. Please try again.</p>
-<?php
-        }
-?>     
-        </div>
-    </main>
-    <footer>
-        <p>&copy; 2020 | Alexandra Derby | Canada | <a href="http://www.byui.edu/online">BYU Idaho Online Learning</a></p>
-    </footer>
-</body>
-</html>
