52c2eccd0d1a39b2cf05b1bbde897f1aa9014b0d
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-30 14:00:07 -0700

    1

--- web/weeklyproves/week6prove/newpasswordconfirmation.php
+++ web/weeklyproves/week6prove/newpasswordconfirmation.php
@@ -10,7 +10,7 @@ var_dump($newpassword2);
 var_dump($artistid);
 $databasepasswordrow = $_SESSION['dbconnection']->query("SELECT password FROM artist_account WHERE artist_id = '$artistid'");
     foreach($databasepasswordrow as $result) {
-        $databasepassword = $result['password']
+        $databasepassword = $result['password'];
     }
 ?>
 <!DOCTYPE html>
