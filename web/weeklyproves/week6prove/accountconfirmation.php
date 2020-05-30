8d376eef605c6ff136be11c90dcc20f6a56988e9
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-05-29 22:10:16 -0700

    fix

--- web/weeklyproves/week6prove/accountconfirmation.php
+++ web/weeklyproves/week6prove/accountconfirmation.php
@@ -12,7 +12,8 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $newpassword = $_POST['newpassword'];
 $stripeaccountid = $_POST['stripeaccountid'];
-$sql = $_SESSION['dbconnection']->query("INSERT INTO artist_account (email, password, stripeaccountid, firstname, lastname, accumulatedsales) VALUES ('$email', '$newpassword', '$stripeaccountid', '$firstname, '$lastname', 0.00)");
+$query = "INSERT INTO artist_account (email, password, stripeaccountid, firstname, lastname, accumulatedsales) VALUES ('$email', '$newpassword', '$stripeaccountid', '$firstname, '$lastname', 0.00)";
+$sql = $_SESSION['dbconnection']->query($query);
 ?>
 <!DOCTYPE html>
 <html lang="en-US">   
