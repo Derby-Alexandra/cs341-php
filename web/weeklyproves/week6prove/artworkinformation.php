57d7a632f7768d1e6678c2271cf8a2e172fa23fe
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-06-02 21:28:09 -0700

    delete updates

--- web/weeklyproves/week6prove/artworkinformation.php
+++ web/weeklyproves/week6prove/artworkinformation.php
@@ -61,7 +61,7 @@ $artworks = [];
                 <p>Price: <?php echo $artwork['price']?></p>
                 <br>
                 <form action="deleteartwork.php" methond="POST">
-                     <input type="hidden" name="artistid" value=<?php echo $artwork['artistid']?>>
+                     <input type="hidden" name="artistid" value=<?php echo $artistid; ?>>
                      <input type="hidden" name="artworkid" value=<?php echo $artwork['artworkid']?>>
                      <button type="submit">Delete</button>
                 </form>
