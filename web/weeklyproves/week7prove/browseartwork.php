e44877351ead96728639dcbf18d223aea2e4411d
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-06-06 13:01:05 -0700

    week 7 updates

--- web/weeklyproves/week7prove/browseartwork.php
+++ web/weeklyproves/week7prove/browseartwork.php
@@ -5,10 +5,10 @@ include 'dbconnect.php';
 $search_results = [];
 if (isset($_POST['searchname'])) {
     $searchname = $_POST['searchname'];
-    $query = "SELECT a.imageurl, a.price, a.description, a.resolution, b.firstname, b.lastname FROM artwork AS a JOIN artist_account AS b ON a.artistid = b.artistid WHERE lower(b.firstname) LIKE lower('%$searchname%') OR lower(b.lastname) LIKE lower('%$searchname%')";
+    $query = "SELECT a.imageurl, a.price, a.description, a.resolution, a.artworkid, b.firstname, b.lastname FROM artwork AS a JOIN artist_account AS b ON a.artistid = b.artistid WHERE lower(b.firstname) LIKE lower('%$searchname%') OR lower(b.lastname) LIKE lower('%$searchname%')";
     $search_results = $_SESSION['dbconnection']->query($query);
 } else {
-    $search_results = $_SESSION['dbconnection']->query("SELECT a.imageurl, a.price, a.description, a.resolution, b.firstname, b.lastname FROM artwork AS a JOIN artist_account AS b ON a.artistid = b.artistid");
+    $search_results = $_SESSION['dbconnection']->query("SELECT a.imageurl, a.price, a.description, a.resolution a.artworkid, b.firstname, b.lastname FROM artwork AS a JOIN artist_account AS b ON a.artistid = b.artistid");
 }
 ?>
 <!DOCTYPE html>
