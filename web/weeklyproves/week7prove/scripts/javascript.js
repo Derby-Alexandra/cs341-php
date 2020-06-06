29d1acc08472975e66ba5cc75f7ce83348f34b5e
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-06-06 14:55:09 -0700

    week 7 updates

--- web/weeklyproves/week7prove/scripts/javascript.js
+++ web/weeklyproves/week7prove/scripts/javascript.js
@@ -3,7 +3,7 @@ for (let i=0; i<addtocart.length; i++) {
     addtocart[i].addEventListener('click', function(event) {
         let cart = localStorage.getItem('cart') || '[]'
         let parsedcart = JSON.parse(cart)
-        parsedcart.push(event.target.dataset.artworkid)
+        parsedcart.push(parseInt(event.target.dataset.artworkid))
         localStorage.setItem('cart', JSON.stringify(parsedcart))
     })
 }
